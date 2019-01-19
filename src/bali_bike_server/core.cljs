f8b48293c217e4cdbb5c4d7aa5f94f8a0a7816c5
Stepan Lusnikov <endenwer@gmail.com>
2019-01-19 12:00:17 +0800

    Convert params to cljs

--- src/bali_bike_server/core.cljs
+++ src/bali_bike_server/core.cljs
@@ -8,28 +8,28 @@
             [promesa.core :as p]))
 
 (def resolvers
-  {:Query {:bikes (fn [_ args context] (.prisma.bikes context))
-           :bike (fn [_ args context] (.prisma.bike context (clj->js {:id (.-id args)})))
-           :bookings (fn [_ args context] (.prisma.bookings context))
-           :booking (fn [_ args context] (.prisma.booking context (clj->js {:id (.-id args)})))}
-   :Mutation {:createBooking (fn [_ args context]
-                               (.prisma.createBooking
-                                context
-                                (clj->js {:startDate (.-startDate args)
-                                          :endDate (.-endDate args)
-                                          :deliveryLocationLongitude (.-deliveryLocationLongitude args)
-                                          :deliveryLocationLatitude (.-deliveryLocationLatitude args)
-                                          :deliveryLocationComment (.-deliveryLocationComment args)
-                                          :deliveryTime (.-deliveryTime args)
-                                          :userUid (.-user.uid context)
-                                          :bike {:connect {:id (.-bikeId args)}}})))}
-   :Bike {:reviews (fn [parent args context]
-                     (.reviews (.prisma.bike context (clj->js {:id (.-id parent)}))))}
-   :Booking {:bike (fn [parent args context]
-                     (.bike (.prisma.booking context (clj->js {:id (.-id parent)}))))}})
+  {:Query {:bikes (fn [_ _ {:keys [prisma]}] (.bikes prisma))
+           :bike (fn [_ _ {:keys [prisma args]}] (.bike prisma #js {:id (:id args)}))
+           :bookings (fn [_ args {:keys [prisma]}] (.bookings prisma))
+           :booking (fn [_ {:keys [id]} {:keys [prisma args]}] (.booking prisma #js {:id (:id args)}))}
+   :Mutation {:createBooking (fn [_ _ {:keys [prisma user args]}]
+                               (.createBooking
+                                prisma
+                                #js {:startDate (:startDate args)
+                                     :endDate (:endDate args)
+                                     :deliveryLocationLongitude (:deliveryLocationLongitude args)
+                                     :deliveryLocationLatitude (:deliveryLocationLatitude args)
+                                     :deliveryLocationComment (:deliveryLocationComment args)
+                                     :deliveryTime (:deliveryTime args)
+                                     :userUid (:uid user)
+                                     :bike {:connect {:id bikeId}}}))}
+   :Bike {:reviews (fn [_ _ {:keys [prisma parent]}]
+                     (.reviews (.bike prisma #js {:id (:id parent)})))}
+   :Booking {:bike (fn [_ _ {:keys [prisma parent]}]
+                     (.bike (.booking prisma #js {:id (:id parent)})))}})
 
 (def is-authenticated
-  ((rule) (fn [parent args context info] (js/Boolean (.-user context)))))
+  ((rule) (fn [parent args context info] (js/Boolean (:user context)))))
 
 (def is-booking-owner
   ((rule) (fn [parrent args context info]
@@ -46,20 +46,30 @@
 
 (defn inject-user
   [resolve root args context info]
-  (if-let [token (.-request.headers.authorization context)]
+  (if-let [token (.-headers.authorization (:request context))]
     (alet [user (p/await (auth/get-user token))
-           new-context (oset! context "!user" user)]
+           new-context (assoc context :user (js->clj user :keywordize-keys true))]
           (resolve root args new-context info))
     (resolve root args context info)))
+
+(defn move-params-to-context
+  [resolve parent args context info]
+  (resolve
+   parent
+   args
+   (merge
+    context
+    {:args (js->clj args :keywordize-keys true)
+     :parent (js->clj parent :keywordize-keys true)})
+   info))
 
 (def server
   (graphql-server. (clj->js {:typeDefs "./schema.graphql"
                              :resolvers (clj->js resolvers)
-                             :middlewares [inject-user permissions]
+                             :middlewares [move-params-to-context inject-user permissions]
                              :context (fn [req]
-                                        (clj->js
-                                         {:prisma (.-prisma prisma-client)
-                                          :request (.-request req)}))})))
+                                        {:prisma (.-prisma prisma-client)
+                                         :request (.-request req)})})))
 
 (defn main []
   (auth/init)
